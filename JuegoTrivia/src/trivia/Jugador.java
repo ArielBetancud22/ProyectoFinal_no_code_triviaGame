9743198949c090bbf9e3a9b3df5f7089706dc46c
Dante Martinez <mzadantemartinez@gmail.com>
2022-10-28 17:37:47 -0300

    Se crea la clases Jugador y test - tambien paquete test y trivia

---
+++ JuegoTrivia/src/trivia/Jugador.java
@@ -0,0 +1,22 @@
+/*
+ * /**
+ * Grupo no_code : JUEGO DE TRIVIA 
+ * INTEGRANTES:
+ * Jeremias Riquero: jereriquero045@gmail.com
+ * Dante N Martinez: niko.mrt@gmail.com
+ * Dario Carrizo: dariocarrizo32@gmail.com
+ * Andres Winckler: andreswinckler@gmail.com 
+ * Abel Pierna: mpierna3@gmail.com
+ * Franco Lara: frankolara2013@gmail.com
+ * Lucas Salinas:  lsaf.serviciotecnico@gmail.com
+ * Ezequiel Mariscal: ezequielramonmariscal@gmail.com
+ */
+package trivia;
+
+/**
+ *
+ * @author no_code
+ */
+public class Jugador {
+    //Aqui el codigo
+}
